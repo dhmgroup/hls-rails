f66fd4b065fe02eeb28b1511dc529609f17857a3
Douglas Harrington Muhone <theeomm@outlook.com>
2018-06-05 21:52:14 +0800

    added hls map

--- lib/hls/rails.rb
+++ lib/hls/rails.rb
@@ -3,6 +3,10 @@ require "hls/rails/version"
 module Hls
     module Rails
         class Engine < ::Rails::Engine
+            
+            # initializer :append_dependent_assets_path, :group => :all do |app|
+            #     app.config.assets.paths += %w( javascript )
+            # end
         end
     end
 end
